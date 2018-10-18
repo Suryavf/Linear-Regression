40027f75ea27c0445714dfe5be2b1cb34cdaae2e
Victor Flores <suryavf@gmail.com>
2018-10-17 21:27:08 -0500

    Add categorical features

--- test.py
+++ test.py
@@ -12,6 +12,8 @@ import seaborn as sns
 from scipy import stats
 from sklearn import metrics
 from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import KFold
 
@@ -26,91 +28,83 @@ dataTest  = pd.concat([ dataTest, dataTTest ],axis = 1)
 
 # Name of columns
 tr = ['SalePrice']
-ca = ['MSSubClass',
-      'MSZoning',
-      'Street',
-      'Alley',
-      'LotShape',
-      'LandContour',
-      'Utilities',
-      'LotConfig',
-      'LandSlope',
-      'Neighborhood',
-      'Condition1',
-      'Condition2',
-      'BldgType',
-      'HouseStyle',
-      'OverallQual',
-      'OverallCond',
-      'RoofStyle',
-      'RoofMatl',
-      'Exterior1st',
-      'Exterior2nd',
-      'MasVnrType',
-      'ExterQual',
-      'ExterCond',
-      'Foundation',
-      'BsmtQual',
-      'BsmtCond',
-      'BsmtExposure',
-      'BsmtFinType1',
-      'BsmtFinType2',
-      'Heating',
-      'HeatingQC',
-      'CentralAir',
-      'Electrical',
-      'BsmtFullBath',
-      'BsmtHalfBath',
-      'FullBath',
-      'HalfBath',
-      'BedroomAbvGr',
-      'YrSold',
-      'SaleType',
-      'SaleCondition']
-nu = ['LotFrontage',
-      'LotArea',
-      'OverallQual',
-      'OverallCond',
-      'YearBuilt',
-      'YearRemodAdd',
-      'MasVnrArea',
-      'BsmtFinSF1',
-      'BsmtFinSF2', 
-      'BsmtUnfSF',
-      'TotalBsmtSF',
-      '1stFlrSF',
-      '2ndFlrSF',
-      'LowQualFinSF',
-      'GrLivArea',
-      'BsmtFullBath',
-      'BsmtHalfBath',
-      'FullBath',
-      'HalfBath',
-      'BedroomAbvGr',
-      'KitchenAbvGr',
-      'TotRmsAbvGrd',
-      'Fireplaces',
-      'GarageYrBlt',
-      'GarageCars',
-      'GarageArea',
-      'WoodDeckSF',
-      'OpenPorchSF',
-      'EnclosedPorch',
-      '3SsnPorch',
-      'ScreenPorch',
-      'PoolArea',
-      'MiscVal',
-      'MoSold',
-      'YrSold',
+
+catOrdinal = ['MSZoning','Street','Alley','LandContour',
+              'Utilities','LotConfig','Neighborhood','Condition1','Condition2',
+              'BldgType','HouseStyle','RoofStyle','RoofMatl','Exterior1st',
+              'Exterior2nd','MasVnrType','Foundation','Heating','CentralAir',
+              'Electrical','Functional','GarageType','MiscFeature','SaleType',
+              'SaleCondition']
+
+catNominal = {'LotShape':['Reg','IR1','IR2','IR3','NA'],
+              'LandSlope':['Gtl','Mod','Sev','NA'],
+              'ExterQual':['Ex','Gd','TA','Fa','Po','NA'],
+              'ExterCond':['Ex','Gd','TA','Fa','Po','NA'],
+              'BsmtQual':['Ex','Gd','TA','Fa','Po','NA'],
+              'BsmtCond':['Ex','Gd','TA','Fa','Po','NA'],
+              'BsmtExposure':['Gd','Av','Mn','No','NA'],
+              'BsmtFinType1':['GLQ','ALQ','BLQ','Rec','LwQ','Unf','NA'],
+              'BsmtFinType2':['GLQ','ALQ','BLQ','Rec','LwQ','Unf','NA'],
+              'HeatingQC':['Ex','Gd','TA','Fa','Po','NA'],
+              'KitchenQual':['Ex','Gd','TA','Fa','Po','NA'],
+              'FireplaceQu':['Ex','Gd','TA','Fa','Po','NA'],
+              'GarageQual':['Ex','Gd','TA','Fa','Po','NA'],
+              'GarageCond':['Ex','Gd','TA','Fa','Po','NA'],
+              'PavedDrive':['Y','P','N'],
+              'PoolQC':['Ex','Gd','TA','Fa','NA'],
+              'Fence':['GdPrv','MnPrv','GdWo','MnWw','NA']}
+
+nu = ['MSSubClass','LotFrontage','LotArea','OverallQual','OverallCond','YearBuilt',
+      'YearRemodAdd','MasVnrArea','BsmtFinSF1','BsmtFinSF2','BsmtUnfSF',
+      'TotalBsmtSF','1stFlrSF','2ndFlrSF','LowQualFinSF','GrLivArea',
+      'BsmtFullBath','BsmtHalfBath','FullBath','HalfBath','BedroomAbvGr',
+      'KitchenAbvGr','TotRmsAbvGrd','Fireplaces','GarageYrBlt','GarageCars',
+      'GarageArea','WoodDeckSF','OpenPorchSF','EnclosedPorch','3SsnPorch',
+      'ScreenPorch','PoolArea','MiscVal','MoSold','YrSold',
       'SalePrice']
+
+
+
+"""
+Categorical to Numerical
+------------------------
+"""
+
+# Ordinal
+for cat in catOrdinal:
+    
+    ## Add to data train
+    dummy =pd.get_dummies(dataTrain[cat],prefix=cat)
+    dataTrain = pd.concat([dataTrain, dummy], axis=1, join_axes=[dataTrain.index])
+    
+    ## Add to data test
+    dummy =pd.get_dummies(dataTest[cat],prefix=cat)
+    dataTest = pd.concat([dataTest, dummy], axis=1, join_axes=[dataTest.index])
+    
+    ## Add to nu
+    nu.extend(dummy.columns.tolist())
+    
+# Nominal
+for cat in catNominal.keys():
+    
+    dom = catNominal[cat]
+    
+    ## Add to data train
+    carNum = [dom.index(x) for x in dataTrain[cat].fillna('NA')  ]
+    
+    
+    
+
+"""
+Data selection
+--------------
+"""
 
 # Pearson correlation
 pearson  = dataTrain[nu].corr('pearson' )
 spearman = dataTrain[nu].corr('spearman')
 pearson  =  pearson[tr]
 spearman = spearman[tr]
-
-# Anova one-way
 
 
 # Data selection
@@ -186,11 +180,11 @@ def BatchGradientDescent(x_train,y_train,x_test):
         exErr = err
         err   = 0
         
+        # Cost function
         y_pred = np.dot(x_train,theta[:-1]) + theta[:][-1]
+        J =  (1.0/n_samples)*  np.dot( x_train.T, y_train - y_pred)
         
-        J =  (1.0/n_samples)*  np.dot( x_train.T,  # Transposing
-                                       y_train - y_pred)
-        
+        # Theta calculation
         for i in range( n_predictor ):
             theta[i] = theta[i] + alpha *np.dot(x_train[:][i],J)  
         
@@ -278,4 +272,64 @@ print('MAE=',np.average(MAE),'\tMSE=',np.average(MSE),'\tRMSE=',np.average(RMSE)
 print('\n')    
 
 
+
+"""
+Random Forest Regressor
+---------------------------
+"""
+
+MAE = list(); MSE = list(); RMSE = list()
+for train, test in kf.split(x):
+    # Select
+    x_train = x[train]; y_train = y[train]
+    x_test  = x[test ]; y_test  = y[test ]
+    
+    # Regression
+    regr = RandomForestRegressor(random_state=0,n_estimators=100)
+    
+    # Train
+    regr.fit(x,y)
+    
+    # Test
+    y_pred = regr.predict( x_test )
+    
+    # Metrics
+    MAE .append(        metrics.mean_absolute_error(y_test, y_pred) )
+    MSE .append(        metrics.mean_squared_error (y_test, y_pred) )
+    RMSE.append(np.sqrt(metrics.mean_squared_error (y_test, y_pred)))
+    
+print('Random Forest Regressor:')
+print('MAE =',np.average(MAE),'\tMSE =',np.average(MSE),'\tRMSE =',np.average(RMSE))
+print('\n')
+
+
+
+"""
+Gradient Boosting Regressor
+---------------------------
+"""
+
+MAE = list(); MSE = list(); RMSE = list()
+for train, test in kf.split(x):
+    # Select
+    x_train = x[train]; y_train = y[train]
+    x_test  = x[test ]; y_test  = y[test ]
+    
+    # Regression
+    regr = GradientBoostingRegressor(random_state=0,n_estimators=100)
+    
+    # Train
+    regr.fit(x,y)
+    
+    # Test
+    y_pred = regr.predict( x_test )
+    
+    # Metrics
+    MAE .append(        metrics.mean_absolute_error(y_test, y_pred) )
+    MSE .append(        metrics.mean_squared_error (y_test, y_pred) )
+    RMSE.append(np.sqrt(metrics.mean_squared_error (y_test, y_pred)))
+    
+print('Gradient Boosting Regressor:')
+print('MAE =',np.average(MAE),'\tMSE =',np.average(MSE),'\tRMSE =',np.average(RMSE))
+print('\n')
 
